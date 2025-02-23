96168d6c49471b031722d842cadf885f4287349b
jmininel <ju.mininel@hotmail.com>
2025-02-23 19:25:14 -0300

    chore: update css variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 /** @format */
 
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20  sm:p-20 font-[family-name:var(--font-geist-sans) gap-16]">
       <Button>oi</Button>
-      <Input placeholder="escreva aqui"></Input>
     </div>
   );
 }
