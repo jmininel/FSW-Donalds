21a76c3bde634c5b195beb361ea137c137e44344
jmininel <ju.mininel@hotmail.com>
2025-02-23 19:31:15 -0300

    chore: update .vscode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20  sm:p-20 font-[family-name:var(--font-geist-sans) gap-16]">
+    <div className="border-red-600 flex">
       <Button>oi</Button>
     </div>
   );
